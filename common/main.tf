1fd0775ce849405444b93d6b17c44e0d7b190b38
Menezes <johnny.dias.menezes@accenture.com>
2023-05-30 14:44:22 -0300

    test works space prod + dev

--- common/main.tf
+++ common/main.tf
@@ -11,3 +11,9 @@ terraform {
 resource "random_pet" "vpc" {}
 resource "random_pet" "s3" {}
 resource "random_pet" "rds" {}
+
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket_prefix = "poc-deploy-dev"
+  
+}
