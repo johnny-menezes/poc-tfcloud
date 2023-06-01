c19737d5eacf211303db52ffea609b9bb71796c7
johnny-menezes <jhojohnny@hotmail.com>
2023-06-01 15:23:56 -0300

    update infra

--- common/main.tf
+++ common/main.tf
@@ -7,19 +7,6 @@ terraform {
   }
 }
 
-
-data "terraform_remote_state" "production" {
-  backend = "remote"
-
-  config = {
-    organization = "tfcloud-organization"
-    workspaces   = {
-      name = "production"
-    }
-  }
-  
-}
-
 resource "random_pet" "vpc" {}
 resource "random_pet" "s3" {}
 resource "random_pet" "rds" {}
@@ -35,7 +22,7 @@ module "vpc-module" {
   source  = "app.terraform.io/tfcloud-organization/vpc-module/aws"
   version = "1.0.5"
   cidr    = var.tfc_subnet_cidr
-  name    = data.terraform_remote_state.production.outputs.vpc_name
+  
 }
 
 variable "tfc_subnet_cidr" {}
