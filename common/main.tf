6fc09af4d01fbd409167851d32448be3b5c42aa5
johnny-menezes <jhojohnny@hotmail.com>
2023-06-01 15:28:27 -0300

    update ifra dev

--- common/main.tf
+++ common/main.tf
@@ -20,9 +20,9 @@ resource "aws_s3_bucket" "mybucket" {
 
 module "vpc-module" {
   source  = "app.terraform.io/tfcloud-organization/vpc-module/aws"
-  version = "1.0.5"
+  version = "1.0.4"
   cidr    = var.tfc_subnet_cidr
-  
+
 }
 
 variable "tfc_subnet_cidr" {}
