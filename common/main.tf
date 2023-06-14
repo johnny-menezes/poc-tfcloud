9af21f68b9dc4e18af99deaee6ac3106c4439012
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-06-14 01:59:01 +0000

    add share state.tf

--- common/main.tf
+++ common/main.tf
@@ -9,6 +9,18 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "production" {
+  backend = "remote"
+
+  config = {
+    organization = "tfcloud-organization"
+    workspaces   = {
+      name = "poc-tfcloud-production"
+    }
+  }
+}
+
+
 resource "random_pet" "vpc" {}
 resource "random_pet" "s3" {}
 resource "random_pet" "rds" {}
@@ -21,8 +33,9 @@ resource "aws_s3_bucket" "mybucket" {
 
 module "vpc-module" {
   source  = "app.terraform.io/tfcloud-organization/vpc-module/aws"
-  version = "1.0.6"
+  version = "1.0.7"
   cidr    = var.tfc_subnet_cidr
+  name    = data.terraform_remote_state.production.outputs.vpc_name
 
 }
 
