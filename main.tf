6ce73f48916a825fd337ff4cc64a4db17d334349
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2024-08-12 21:20:46 +0000

    update

--- main.tf
+++ main.tf
@@ -10,11 +10,14 @@ terraform {
 }
 
 
-variable "region" {}
+variable "region" {
+  default = "us-east-1"
+}
 
 provider "aws" {
   region = var.region
 }
+
 resource "aws_s3_bucket" "bucket-poc-tf-cloud" {
   bucket = "bucket-poc-tf-cloud"
 }
