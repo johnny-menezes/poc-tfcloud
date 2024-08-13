8a223b4319dce5ba2aa16361d32ca5792c994726
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2024-08-13 21:01:58 +0000

    test output module

--- output.tf
+++ output.tf
@@ -2,21 +2,21 @@
 
 output "arn" {
   description = "AWS SecretManager Secret ARN"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.arn
+  value       = module.asm.aws_secretsmanager_secret.secret_demo.arn
 }
 
 output "id" {
   description = "AWS SecretManager Secret ARN"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.id
+  value       = module.asm.aws_secretsmanager_secret.secret_demo.id
 }
 
 output "secret" {
   description = "AWS SecretManager Secret resource"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo
+  value       = module.asm.aws_secretsmanager_secret.secret_demo
 }
 
 output "secret_version" {
   description = "AWS SecretManager Secret Version resource"
-  value       = module.asm_gopoints.aws_secretsmanager_secret_version.secret_demo
+  value       = module.asm.aws_secretsmanager_secret_version.secret_demo
   sensitive   = true
 }
