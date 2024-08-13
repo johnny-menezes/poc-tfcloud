3f467abbea96e6263be9529dbacd65c66304b231
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2024-08-13 21:30:14 +0000

    blibli

--- output.tf
+++ output.tf
@@ -1,22 +1,46 @@
 ##### output secretsmanager ########
 
+#output "arn" {
+#  description = "AWS SecretManager Secret ARN"
+#  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.arn
+#}
+#
+#output "id" {
+#  description = "AWS SecretManager Secret ARN"
+#  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.id
+#}
+#
+#output "secret" {
+#  description = "AWS SecretManager Secret resource"
+#  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo
+#}
+#
+#output "secret_version" {
+#  description = "AWS SecretManager Secret Version resource"
+#  value       = module.asm_gopoints.aws_secretsmanager_secret_version.secret_demo
+#  sensitive   = true
+#}
+
+
+
+
 output "arn" {
   description = "AWS SecretManager Secret ARN"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.arn
+  value       = module.asm_gopoints.secret_demo.arn
 }
 
 output "id" {
   description = "AWS SecretManager Secret ARN"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo.id
+  value       = module.asm_gopoints.secret_demo.id
 }
 
 output "secret" {
   description = "AWS SecretManager Secret resource"
-  value       = module.asm_gopoints.aws_secretsmanager_secret.secret_demo
+  value       = module.asm_gopoints.secret_demo
 }
 
 output "secret_version" {
   description = "AWS SecretManager Secret Version resource"
-  value       = module.asm_gopoints.aws_secretsmanager_secret_version.secret_demo
+  value       = module.asm_gopoints.secret_demo
   sensitive   = true
 }
